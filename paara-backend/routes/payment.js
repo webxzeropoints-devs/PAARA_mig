65342ac1c77a5b5349cdb4345f1d4d26ba286385
webxzeropoints-devs <webxzeropoints@gmail.com>
2026-09-11 22:23:13 +0530

    Update payment.js

--- paara-backend/routes/payment.js
+++ paara-backend/routes/payment.js
@@ -64,8 +64,6 @@ async function markOrderPaid(orderId, paymentReference) {
   }
 }
 
-}
-
 async function sendPaidInvoice(orderId) {
   const { rows: orderRows } = await db.query(`
     SELECT o.*, c.email, c.name
